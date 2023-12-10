53cf589764223d6d3a8edd409de996311af2f0e4
muhyasin89 <muhyasin89@gmail.com>
2023-12-10 14:52:37 +0700

    [main]add first init

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './src/components/Hello';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Hello name={'scumbag'} />
       <StatusBar style="auto" />
     </View>
   );
