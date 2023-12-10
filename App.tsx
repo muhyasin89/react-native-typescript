84fff29c2a274042ccca5f0eeb5b86a590162215
muhyasin89 <muhyasin89@gmail.com>
2023-12-10 15:19:49 +0700

    [main]add checkpoint

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Hello from './src/components/Hello';
+import "./nativewind-output"
+import { NativeWindStyleSheet } from "nativewind";
+
+NativeWindStyleSheet.setOutput({
+  default: "native",
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
+       <Text className="text-gray-400 text-2xltext-md font-bold mt-2">
+            Open up App.js to start working on your app!
+        </Text>
       <Hello name={'scumbag'} />
       <StatusBar style="auto" />
     </View>
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+const nativewind = require("nativewind/tailwind/native")
+
+module.exports = {
+  content: [
+    "./App.{js,jsx,ts,tsx}", "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'blue': '#1fb6ff',
+        'purple': '#7e5bef',
+        'gray': '#8492a6',
+            },
+     spacing: {
+         '2x': '20px',
+         '3x': '30px',
+            },
+     borderRadius: {
+         '4xl': '40px',
+            }
+        }
+    },
+  plugins: [
+    nativewind()
+  ],
+}
+
